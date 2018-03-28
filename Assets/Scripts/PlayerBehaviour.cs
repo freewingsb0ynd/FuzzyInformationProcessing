751b1a5df5404b564812d9a8ebe1a738a0a5060c
hoangelato <lhp.huyhoangnguyen@gmail.com>
2018-03-28 14:20:56 +0700

    merge like idiots

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -5,6 +5,7 @@ using UnityEngine.AI;
 
 public class PlayerBehaviour : MonoBehaviour {
     public GameObject target;
+	public float angleSpeed = 1.0f;
     NavMeshAgent agent;
 
 	// Use this for initialization
@@ -17,5 +18,7 @@ public class PlayerBehaviour : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         agent.SetDestination(target.transform.position);
+		Vector3 direction = new Vector3 (agent.steeringTarget.x, agent.transform.position.y, agent.steeringTarget.z) - agent.transform.position;
+		if (direction != Vector3.zero) agent.transform.rotation = Quaternion.Slerp(agent.transform.rotation, Quaternion.LookRotation(direction), angleSpeed*Time.deltaTime);
 	}
 }
