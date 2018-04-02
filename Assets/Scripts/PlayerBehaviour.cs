9784705d0920d62c5ebc1e2e643b5f9e4f5ba9fe
hoangelato <lhp.huyhoangnguyen@gmail.com>
2018-04-02 14:49:55 +0700

    navmesh edit

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -5,12 +5,13 @@ using UnityEngine.AI;
 
 public class PlayerBehaviour : MonoBehaviour {
     public GameObject target;
-	public float angleSpeed = 1.0f;
+    public GameObject camera;
+	//public float angleSpeed = 1.0f;
     NavMeshAgent agent;
-
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         agent = gameObject.GetComponent<NavMeshAgent>();
+        
 	}
 	
     
@@ -18,7 +19,7 @@ public class PlayerBehaviour : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         agent.SetDestination(target.transform.position);
-		Vector3 direction = new Vector3 (agent.steeringTarget.x, agent.transform.position.y, agent.steeringTarget.z) - agent.transform.position;
-		if (direction != Vector3.zero) agent.transform.rotation = Quaternion.Slerp(agent.transform.rotation, Quaternion.LookRotation(direction), angleSpeed*Time.deltaTime);
+		//Vector3 direction = new Vector3 (agent.steeringTarget.x, agent.transform.position.y, agent.steeringTarget.z) - agent.transform.position;
+		//if (direction != Vector3.zero) agent.transform.rotation = Quaternion.Slerp(agent.transform.rotation, Quaternion.LookRotation(direction), angleSpeed*Time.deltaTime);
 	}
 }
